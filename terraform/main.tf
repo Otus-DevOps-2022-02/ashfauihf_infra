2791f8820ceb6df0b8de48a9244184305b1700ea
ashfauihf <asz81.ru@gmail.com>
2022-04-11 14:48:27 +0000

    delete required_version  main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,6 @@
    # required_version = ">= 0.35"
 #}
 provider "yandex" {
-  required_version = ">= 0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
