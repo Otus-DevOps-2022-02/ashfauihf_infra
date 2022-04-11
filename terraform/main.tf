c1a6bc999b1f3ffdd1af5bbe899a9adf5d2785b7
ashfauihf <asz81.ru@gmail.com>
2022-04-11 14:45:11 +0000

    commit terraform lines main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud\yandex"
-    }
-  }
-  required_version = ">= 0.35"
-}
+#terraform {
+ #required_providers {
+   #yandex = {
+     #source  = "yandex-cloud/yandex"
+    #}
+  #}
+   # required_version = ">= 0.35"
+#}
 provider "yandex" {
+  required_version = ">= 0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
