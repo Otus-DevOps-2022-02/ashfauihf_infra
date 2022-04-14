3ed22f3103fd363590524e26be85406f3d15b748
ashfauihf <asz81.ru@gmail.com>
2022-04-14 12:51:49 +0000

    edit terraform4

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,23 +1,13 @@
-#terraform {
-  #required_providers {
-    #yandex = {
-     # source = "yandex-cloud/yandex"
-    #}
-  #}
+terraform {
  backend "s3" {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "storage-backet"
     region     = "ru-central1"
     key        = "terraform.tfstate"
+    access_key = "YCAJEZP2RUhtNBFtwj1qH84e5"
+    secret_key = "YCNLqNe_lX3vd2C1en-diUEvFzfPL78P4BbSiqeh"
 
     skip_region_validation      = true
     skip_credentials_validation = true
-  }
-}
-
-provider "yandex" {
-  token     = "AQAAAAAf1LcMAATuwdh9T_fk7U_7oRISc4znA5U"
-  cloud_id  = "b1gluqn84cqgcm88c231"
-  folder_id = "b1gp43annqsmtvmv5lmn"
-  zone      = "ru-central1-a"
+ }
 }
