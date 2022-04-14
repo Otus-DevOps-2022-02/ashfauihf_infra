c233eca1841c2a9963ed36bdcba957c214ee8fc2
ashfauihf <asz81.ru@gmail.com>
2022-04-14 13:49:08 +0000

    edit packer jsons file

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -24,11 +24,11 @@ variable "subnet_id" {
 variable "service_key_file" {
   description = "terrafkey.json"
 }
-variable app_disk_image {
+variable "app_disk_image" {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default     = "reddit-app-base"
 }
-variable db_disk_image {
+variable "db_disk_image" {
   description = "Disk image for reddit db"
-  default = "reddit-db-base"
+  default     = "reddit-db-base"
 }
